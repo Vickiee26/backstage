722c5bfa730c15f76978ff8805e1fb27b131a67b
Vigneswara Harichandran <32336421+Vickiee26@users.noreply.github.com>
2023-01-23 13:14:10 +0530

    Reverted App.tsx changes
    Signed-off-by: Vigneswara Harichandran <32336421+Vickiee26@users.noreply.github.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -258,6 +258,7 @@ const routes = (
     <Route path="/search" element={<SearchPage />}>
       {searchPage}
     </Route>
+    <Route path="/cost-insights" element={<CostInsightsPage />} />
     <Route
       path="/cost-insights/investigating-growth"
       element={<CostInsightsProjectGrowthInstructionsPage />}
